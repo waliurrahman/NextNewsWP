9cc7d0f9a99d2fe8c84ae575222015f1330b0559
waliurrahman <mwaliurrahman@gmail.com>
2023-02-13 19:51:48 +0600

    Removed Prisma From Project

--- next.config.js
+++ next.config.js
@@ -1,17 +1,9 @@
-const { PrismaClient } = require('@prisma/client')
-const prismaClient = new PrismaClient()
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['images.pexels.com', 'scx1.b-cdn.net', 'news.tagbracket.com'],
   },
-  // env: {
-  //   PRISMA_ENDPOINT: process.env.PRISMA_ENDPOINT,
-  //   PRISMA_SECRET: process.env.PRISMA_SECRET,
-  //   PRISMA_CLIENT: prismaClient,
-  // },
 }
 
 module.exports = nextConfig
