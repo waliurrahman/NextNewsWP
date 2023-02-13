36a818501ddee3f2fee22eb9d212846c05cec6a5
waliurrahman <mwaliurrahman@gmail.com>
2023-02-13 21:31:55 +0600

    Fixed Blur on Image Placeholder

--- src/components/NewsImage.js
+++ src/components/NewsImage.js
@@ -2,25 +2,25 @@ import Image from "next/image"
 
 const NewsImage = ({ media, newsPost, width, height, quality }) => {
 
-  const convertImage = (w, h) => `
-    <svg width="${w}" height="${h}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-      <defs>
-        <linearGradient id="g">
-          <stop stop-color="#333" offset="20%" />
-          <stop stop-color="#222" offset="50%" />
-          <stop stop-color="#333" offset="70%" />
-        </linearGradient>
-      </defs>
-      <rect width="${w}" height="${h}" fill="#333" />
-      <rect id="r" width="${w}" height="${h}" fill="url(#g)" />
-      <animate xlink:href="#r" attributeName="x" from="-${w}" to="${w}" dur="1s" repeatCount="indefinite"  />
-    </svg>`;
+  // const convertImage = (w, h) => `
+  //   <svg width="${w}" height="${h}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+  //     <defs>
+  //       <linearGradient id="g">
+  //         <stop stop-color="#333" offset="20%" />
+  //         <stop stop-color="#222" offset="50%" />
+  //         <stop stop-color="#333" offset="70%" />
+  //       </linearGradient>
+  //     </defs>
+  //     <rect width="${w}" height="${h}" fill="#333" />
+  //     <rect id="r" width="${w}" height="${h}" fill="url(#g)" />
+  //     <animate xlink:href="#r" attributeName="x" from="-${w}" to="${w}" dur="1s" repeatCount="indefinite"  />
+  //   </svg>`;
 
 
-  const toBase64 = (str) =>
-    typeof window === 'undefined'
-      ? Buffer.from(str).toString('base64')
-      : window.btoa(str);
+  // const toBase64 = (str) =>
+  //   typeof window === 'undefined'
+  //     ? Buffer.from(str).toString('base64')
+  //     : window.btoa(str);
 
   return (
 
@@ -35,9 +35,10 @@ const NewsImage = ({ media, newsPost, width, height, quality }) => {
           style={{ color: 'red', height: 'fit-content' }}
           quality={quality ? quality : '40'}
           placeholder="blur"
-          blurDataURL={`data:image/svg+xml;base64,${toBase64(
-            convertImage(700, 475)
-          )}`}
+          blurDataURL="L6J[b+g4x]xGt8bHbHf6.TnhR5S#"
+          // blurDataURL={`data:image/svg+xml;base64,${toBase64(
+          //   convertImage(400, 300)
+          // )}`}
         />
       ))
       : <Image
